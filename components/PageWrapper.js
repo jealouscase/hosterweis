9cec292b3f7c5e38cc51da59d98e22873cda0cc0
jealouscase <henryosterweis@gmail.com>
2024-04-16 15:44:03 -0400

    add name to title

--- components/PageWrapper.js
+++ components/PageWrapper.js
@@ -5,10 +5,10 @@ const PageWrapper = ({ children, pageTitle }) => {
         <div className="p-24 text-3xl max-sm:p-10">
             <HomeLink />
             <br></br><br></br>
-            <h1 className="text-5xl max-sm:text-4xl mb-6">{pageTitle}</h1>
+            <h1 className="text-5xl max-sm:text-4xl mb-6">{pageTitle}<span className="text-2xl ml-4">by Henry Osterweis</span></h1>
             <div className="max-sm:text-xl">{children}</div>
         </div>
     );
 }
- 
+
 export default PageWrapper;
